95f17962f9e18197455d84d0383564e45b45be5e
Vincent Bergeron <vincentbergeron33@gmail.com>
2023-02-20 16:47:08 +0000

    Add onclick for score, periode and shots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Data to be at 0 and time to be at 10 mins when the page loads
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("timer").innerHTML = '10:00';
+    document.getElementById("timer").innerHTML = '5:00';
     document.getElementById("goal-team-1").innerHTML = 0;
     document.getElementById("period-number").innerHTML = 0;
     document.getElementById("goal-team-2").innerHTML = 0;
@@ -10,3 +10,116 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("rooster-details-team-1").innerHTML = '';
     document.getElementById("rooster-details-team-2").innerHTML = '';
 })
+
+/**
+ * The function will increment the score of team 1
+ */
+function incrementScoreTeamOne() {
+    let i = parseInt(document.getElementById("goal-team-1").innerHTML);
+    i++;
+    document.getElementById("goal-team-1").innerHTML = i;
+}
+
+function reduceScoreTeamOne() {
+    let i = parseInt(document.getElementById("goal-team-1").innerHTML);
+    if (i !== 0) {
+        i--;
+    } else { 
+        alert('Impossible!');
+    }
+    document.getElementById("goal-team-1").innerHTML = i;
+}
+
+function incrementPeriod() {
+    let i = parseInt(document.getElementById("period-number").innerText);
+    i++;
+    document.getElementById("period-number").innerHTML = i;
+
+    if (i==1) {
+        /* Timmer = 10 mins*/
+        alert('Beginning of Period 1!')
+    } else if (i==2) {
+        /* Timmer = 10 mins*/
+        alert('Beginning of Period 2!')
+    } else if (i==3) {
+        /* Timmer = 10 mins*/
+        alert('Beginning of Period 3!')
+    } else if (i==4) {
+        /* Timmer = 5 mins*/
+        alert('Beginning of Overtime!')
+    }
+}
+
+function reducePeriod() {
+    let i = parseInt(document.getElementById("period-number").innerHTML);
+    if (i !== 1) {
+        i--;
+    } else { 
+        alert('Impossible!');
+    }
+
+    document.getElementById("period-number").innerHTML = i;
+
+    if (i==1) {
+        /* Timmer = 10 mins*/
+        alert('Beginning of Period 1!')
+    } else if (i==2) {
+        /* Timmer = 10 mins*/
+        alert('Beginning of Period 2!')
+    } else if (i==3) {
+        /* Timmer = 10 mins*/
+        alert('Beginning of Period 3!')
+    } else if (i==4) {
+        /* Timmer = 5 mins*/
+        alert('Beginning of Overtime!')
+    }
+
+}
+
+function incrementScoreTeamTwo() {
+    let i = parseInt(document.getElementById("goal-team-2").innerHTML);
+    i++;
+    document.getElementById("goal-team-2").innerHTML = i;
+}
+
+function reduceScoreTeamTwo() {
+    let i = parseInt(document.getElementById("goal-team-2").innerHTML);
+    if (i !== 0) {
+        i--;
+    } else { 
+        alert('Impossible!');
+    }
+    document.getElementById("goal-team-2").innerHTML = i;
+}
+
+function incrementShotTeamOne() {
+    let i = parseInt(document.getElementById("shot-team-1-number").innerHTML);
+    i++;
+    document.getElementById("shot-team-1-number").innerHTML = i;
+}
+
+function reduceShotTeamOne() {
+    let i = parseInt(document.getElementById("shot-team-1-number").innerHTML);
+    if (i !== 0) {
+        i--;
+    } else { 
+        alert('Impossible!');
+    }
+    document.getElementById("shot-team-1-number").innerHTML = i;
+}
+
+function incrementShotTeamTwo() {
+    let i = parseInt(document.getElementById("shot-team-2-number").innerHTML);
+    i++;
+    document.getElementById("shot-team-2-number").innerHTML = i;
+}
+
+function reduceShotTeamTwo() {
+    let i = parseInt(document.getElementById("shot-team-2-number").innerHTML);
+    if (i !== 0) {
+        i--;
+    } else { 
+        alert('Impossible!');
+    }
+    document.getElementById("shot-team-2-number").innerHTML = i;
+}
